ec76ecc15aad10d3679c0597c0fa3940326c0a96
ShevinuM <nsmcooray@mun.ca>
2023-05-18 17:29:32 +0530

    Major changes related to load/save

---
+++ src/main/java/com/example/controller/PasswordLoadController.java
@@ -0,0 +1,42 @@
+package com.example.controller;
+
+import com.example.interfaces.PasswordLoadCallback;
+import com.example.storage.PasswordEntry;
+import com.example.storage.SerializationPasswordStorage;
+import java.util.List;
+
+/**
+ * The controller for loading passwords.
+ */
+public class PasswordLoadController implements PasswordLoadCallback {
+
+    private final SerializationPasswordStorage passwordStorage = new SerializationPasswordStorage();
+
+    /**
+     * Called when the password loading is successful.
+     * @param passwordEntries The list of password entries loaded successfully.
+     */
+    @Override
+    public void onPasswordLoadSuccess(List<PasswordEntry> passwordEntries) {
+
+    }
+
+    /**
+     * Called when an error occurs during password loading.
+     * @param errorMessage The error message describing the cause of the error.
+     */
+    @Override
+    public void onPasswordLoadError(String errorMessage) {
+
+    }
+
+    /**
+     * Loads the password entries from the password storage.
+     * @return The password storage object.
+     */
+    public SerializationPasswordStorage loadPasswordEntries() {
+        passwordStorage.loadPasswords(this);
+        return passwordStorage;
+    }
+
+}
---
+++ src/main/java/com/example/controller/PasswordSaveController.java
@@ -0,0 +1,21 @@
+package com.example.controller;
+
+import com.example.interfaces.PasswordSaveCallback;
+
+public class PasswordSaveController implements PasswordSaveCallback {
+
+
+
+    @Override
+    public void onPasswordSaveSuccess() {
+
+    }
+
+    @Override
+    public void onPasswordSaveError(String errorMessage) {
+
+    }
+
+
+
+}
